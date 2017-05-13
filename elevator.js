138bf934c19cd9146b41b3573f52b4189ccdf923
Kenia Farias <11431kenia@gmail.com>
2017-05-13 05:34:05 -0600

    updtaed elevator challenge

--- elevator.js
+++ elevator.js
@@ -1,79 +1,80 @@
- class Elevator {
+export default class Elevator {
   constructor() {
-    this.currentFloor = 0;
-    this.requests = [];
-    this.riders = [];
-    this.floorTotal = 0;
-    this.state = 'idle'
-    this.stops = 0;
-  }
-
-  moveUp() {
-    this.countFloorTotal()
-    this.setStatus('moving')
-    return this.currentFloor++
-  }
-
-  moveDown() {
-    this.countFloorTotal()
-    this.setStatus('moving')
-    return this.currentFloor--
-  }
-
-  // stop() {
-  //   this.setStatus('broken')
-  //   this.stopsCounter()
-  // }
-
-  setStatus(string) {
-    switch(string) {
-      case 'moving':
-        return this.state = string;
-      case 'broken':
-        return this.state = string;
-      default:
-        return this.state = 'idle'
-    }
-  }
-
-  addRider(person) {
-    return this.riders.push(person.name)
-  }
-
-  addRequest(person) {
-    return this.requests.push(person)
-  }
-
-  requestFloor(person) {
-    this.addRequest(person);
-  }
-
-  countFloorTotal() {
-    return this.floorTotal++
-  }
-
-  stopsCounter() {
-    this.stops++
+    this.requests = []
+    this.completedRequests = 0
+    this.riders = []
+    this.completedRides = []
+    this.dropOffFloor = 0
+    this.currentFloor = 0
+    this.totalStops = 0
+    this.totalFloors = 0
+    this.motionStatus
   }
 
   reset() {
-    this.currentFloor = 0;
-    this.requests = [];
-    this.riders = [];
-    this.floorTotal = 0;
-    this.state = 'idle';
-    this.stops = 0;
- }
-}
+    this.requests = []
+    this.completedRequests = 0
+    this.riders = []
+    this.completedRides = []
+    this.dropOffFloor = 0
+    this.currentFloor = 0
+    this.totalStops = 0
+    this.totalFloors = 0
+    this.motionStatus
+  }
 
-class Person {
-  constructor(name, currentFloor, requestedFloor) {
-    this.name = name;
-    this.request = {
-      currentFloor: currentFloor,
-      requestedFloor: requestedFloor
+  goToFloor(person) {
+    this.requests.push(person)
+    this.startElevatorTrip(person)
+  }
+
+  startElevatorTrip(person){
+    this.riders.push(person)
+    this.requests.shift()
+    this.motionStatus = 'moving'
+    this.logElevatorTrip()
+    this.stopElevator()
+  }
+
+  logElevatorTrip(){
+    let currentRider = this.riders[0]
+    this.countStops(this.getStops())
+    this.countFloors(this.getStops())
+    this.currentFloor = currentRider.dropOffFloor
+  }
+
+  stopElevator(){
+    this.motionStatus = 'idle'
+    if (this.riders[0].dropOffFloor === this.currentFloor) {
+      this.completedRides.push(this.riders[0])
+      this.riders.shift()
+    }
+    this.completedRequests ++
+  }
+
+  getStops() {
+    return [this.riders[0].currentFloor, this.riders[0].dropOffFloor]
+  }
+
+  countFloors(stopsArray){
+    if(this.currentFloor === 0) {
+      let floordiff1 = stopsArray[0]
+      let floordiff2 = Math.abs(stopsArray[1] - stopsArray[0])
+      let floorsTraversed = floordiff1 + floordiff2
+      this.totalFloors = this.currentFloor + floorsTraversed
+    } else {
+      let floordiff1 = Math.abs(this.currentFloor - stopsArray[0])
+      let floordiff2 = Math.abs(stopsArray[0] - stopsArray[1])
+      let floorsTraversed = floordiff1 + floordiff2
+      this.totalFloors = this.totalFloors + floorsTraversed
+    }
+  }
+
+  countStops(stopsArray){
+    if(this.currentFloor === stopsArray[0]) {
+      this.totalStops = this.totalStops + 1
+    } else {
+      this.totalStops = this.totalStops + 2
     }
   }
 }
-
-module.exports = {Elevator, Person};
--- tests/elevator_test.js
+++ tests/elevator_test.js
@@ -1,53 +1,186 @@
-const assert = require('assert');
-const {Elevator, Person} = require('../elevator');
+const assert = require('assert')
 
 require('babel-core/register')({
   ignore: /node_modules\/(?!ProjectB)/
-});
+})
+
+const Elevator = require('../Elevator').default;
+const Person = require('../Person').default;
 
 describe('Elevator', function() {
-  const elevator = new Elevator();
-  const sarah = new Person('Sarah', 3, 5);
-  const kenia = new Person('Kenia', 6, 2)
+  let elevator = new Elevator()
 
   afterEach(function() {
-  elevator.reset();
+    elevator.reset()
   });
 
-  it('should be able to moveUp and change the currentFloor', function() {
-    elevator.moveUp();
-    assert.equal(elevator.currentFloor, 1);
-  });
+  it('should bring a rider to a floor above their current floor', () => {
+    let mockUser = { name: "Brittany", currentFloor: 2, dropOffFloor: 5 }
 
-  it('should be able to moveUp and change the currentFloor', function() {
-    elevator.currentFloor = 2;
-    assert.equal(elevator.currentFloor, 2);
-    elevator.moveDown();
-    assert.equal(elevator.currentFloor, 1);
-  });
+    elevator.goToFloor(mockUser)
 
-  it('should be able to add a rider to its riders array', function() {
-    elevator.addRider(sarah);
-    assert.equal(elevator.riders[0], 'Sarah');
-  });
+    assert.equal(elevator.currentFloor, 5)
+    assert.equal(elevator.motionStatus, 'idle')
+  })
 
-  it('should be able to add a request to the requests array', function() {
-    elevator.addRequest(kenia);
-    assert.equal(elevator.requests[0].request.currentFloor, 6);
-  });
+  it('should bring a rider to a floor below their current floor', () => {
+    let mockUser2 = { name: "Susan", currentFloor: 8, dropOffFloor: 3 }
 
-  it('a person should be able to request a floor', function() {
-  elevator.requestFloor(sarah);
-  console.log(sarah);
-  assert.equal(elevator.requests[0].request.currentFloor, 3);
-  assert.equal(elevator.requests[0].request.requestedFloor, 5);
-  assert.equal(elevator.requests.length, 1);
-  });
+    elevator.goToFloor(mockUser2)
 
-  it('should be able to count the total number of floors traveled', function() {
-  elevator.moveUp();
-  elevator.moveUp();
-  elevator.moveDown();
-  assert.equal(elevator.floorTotal, 3);
-});
-});
+    assert.equal(elevator.currentFloor, 3)
+    assert.equal(elevator.motionStatus, 'idle')
+  })
+
+  it('should bring a rider to a floor above their current floor', () => {
+    let mockUser3 = { name: "Pammy", currentFloor: 2, dropOffFloor: 4 }
+    assert.equal(elevator.currentFloor, 0)
+
+    elevator.goToFloor(mockUser3)
+
+    assert.equal(elevator.currentFloor, 4)
+    assert.equal(elevator.totalStops, 2)
+    assert.equal(elevator.totalFloors, 4)
+    assert.equal(elevator.motionStatus, 'idle')
+  })
+
+  it('should bring a rider to a floor below their current floor', () => { let mockUser = { name: "Pammy", currentFloor: 10, dropOffFloor: 2 };
+    let mockUser4 = { name: "Pammy", currentFloor: 10, dropOffFloor: 2 }
+
+    elevator.goToFloor(mockUser4)
+
+    assert.equal(elevator.currentFloor, 2)
+    assert.equal(elevator.motionStatus, 'idle')
+  })
+
+  it('should let multiple people request drop off floors', () => {
+    let Bob = { name: "Bob", currentFloor: 3, dropOffFloor: 9 }
+    let Sue = { name: "Sue", currentFloor: 6, dropOffFloor: 2 }
+
+    elevator.goToFloor(Bob)
+    assert.equal(elevator.currentFloor, 9)
+
+    elevator.goToFloor(Sue)
+    assert.equal(elevator.currentFloor, 2)
+  })
+
+  it('should correctly count floors and stops when PersonA and PersonB go up', () => {
+    let personA = { name: "Bob", currentFloor: 2, dropOffFloor: 4 }
+    let personB = { name: "Sue", currentFloor: 5, dropOffFloor: 10 }
+    assert.equal(elevator.currentFloor, 0)
+
+    elevator.goToFloor(personA)
+    assert.equal(elevator.totalStops, 2)
+    assert.equal(elevator.totalFloors, 4)
+    assert.equal(elevator.currentFloor, 4)
+    assert.equal(elevator.completedRequests, 1)
+
+    elevator.goToFloor(personB)
+    assert.equal(elevator.totalStops, 4)
+    assert.equal(elevator.totalFloors, 10)
+    assert.equal(elevator.currentFloor, 10)
+    assert.equal(elevator.completedRequests, 2)
+  })
+
+  it('should correctly count floors and stops when PersonA goes up and PersonB goes down', () => {
+    let personA = { name: "Bob", currentFloor: 3, dropOffFloor: 9 }
+    let personB = { name: "Sue", currentFloor: 6, dropOffFloor: 2 }
+    assert.equal(elevator.currentFloor, 0)
+    elevator.goToFloor(personA)
+    assert.equal(elevator.totalStops, 2)
+    assert.equal(elevator.totalFloors, 9)
+    assert.equal(elevator.currentFloor, 9)
+    assert.equal(elevator.completedRequests, 1)
+
+    elevator.goToFloor(personB)
+    assert.equal(elevator.totalStops, 4)
+    assert.equal(elevator.totalFloors, 16)
+    assert.equal(elevator.currentFloor, 2)
+    assert.equal(elevator.completedRequests, 2)
+  })
+
+  it('should correctly count floors and stops when PersonA goes down and PersonB goes up', () => {
+    let personA = { name: "Bob", currentFloor: 10, dropOffFloor: 2 }
+    let personB = { name: "Sue", currentFloor: 1, dropOffFloor: 4 }
+    assert.equal(elevator.currentFloor, 0)
+
+    elevator.goToFloor(personA)
+    assert.equal(elevator.totalStops, 2)
+    assert.equal(elevator.totalFloors, 18)
+    assert.equal(elevator.currentFloor, 2)
+    assert.equal(elevator.completedRequests, 1)
+
+    elevator.goToFloor(personB)
+    assert.equal(elevator.totalStops, 4)
+    assert.equal(elevator.totalFloors, 22)
+    assert.equal(elevator.currentFloor, 4)
+    assert.equal(elevator.completedRequests, 2)
+  })
+
+  it('should correctly count floors and stops when PersonA and PersonB go down', () => {
+    let personA = { name: "Bob", currentFloor: 12, dropOffFloor: 2 }
+    let personB = { name: "Sue", currentFloor: 8, dropOffFloor: 4 }
+    assert.equal(elevator.currentFloor, 0)
+
+    elevator.goToFloor(personA)
+    assert.equal(elevator.totalStops, 2)
+    assert.equal(elevator.totalFloors, 22)
+    assert.equal(elevator.currentFloor, 2)
+    assert.equal(elevator.completedRequests, 1)
+
+    elevator.goToFloor(personB)
+    assert.equal(elevator.totalStops, 4)
+    assert.equal(elevator.totalFloors, 32)
+    assert.equal(elevator.currentFloor, 4)
+    assert.equal(elevator.completedRequests, 2)
+  })
+
+  it('should correctly count floors and stops when PersonA and PersonB share floors', () => {
+    let personA = { name: "Bob", currentFloor: 4, dropOffFloor: 10 }
+    let personB = { name: "Sue", currentFloor: 10, dropOffFloor: 2 }
+    let personC = { name: "Brittany", currentFloor: 2, dropOffFloor: 12 }
+    let personD = { name: "Robbie", currentFloor: 12, dropOffFloor: 18 }
+
+    assert.equal(elevator.currentFloor, 0)
+
+    elevator.goToFloor(personA)
+    assert.equal(elevator.totalStops, 2)
+    assert.equal(elevator.totalFloors, 10)
+    assert.equal(elevator.currentFloor, 10)
+    assert.equal(elevator.completedRequests, 1)
+
+    elevator.goToFloor(personB)
+    assert.equal(elevator.totalStops, 3)
+    assert.equal(elevator.totalFloors, 18)
+    assert.equal(elevator.currentFloor, 2)
+    assert.equal(elevator.completedRequests, 2)
+
+    elevator.goToFloor(personC)
+    assert.equal(elevator.totalStops, 4)
+    assert.equal(elevator.totalFloors, 28)
+    assert.equal(elevator.currentFloor, 12)
+    assert.equal(elevator.completedRequests, 3)
+
+    elevator.goToFloor(personD)
+    assert.equal(elevator.totalStops, 5)
+    assert.equal(elevator.totalFloors, 34)
+    assert.equal(elevator.currentFloor, 18)
+    assert.equal(elevator.completedRequests, 4)
+  })
+})
+
+
+describe('Person', function() {
+ it('should instantiate a new Person with current floor logged', () => {
+   let person = new Person({name: "Pamela", currentFloor: 2})
+
+   assert.deepEqual(person, { name: 'Pamela', currentFloor: 2 })
+ })
+
+ it('should have a method that allows users to request a floor', () => {
+   let person = new Person({name: "Pamela", currentFloor: 2})
+   person.requestDropOffFloor(10)
+
+   assert.deepEqual(person, { name: 'Pamela', currentFloor: 2, dropOffFloor: 10 })
+ })
+})
